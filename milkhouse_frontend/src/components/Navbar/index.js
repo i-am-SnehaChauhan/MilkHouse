30a561b35f855ba541cb7eabc23fef7a842a827b
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-05-30 17:06:36 +0000

    Navbar complete

--- milkhouse_frontend/src/components/Navbar/index.js
+++ milkhouse_frontend/src/components/Navbar/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements.js'
+import {Nav, NavbarContainer, NavLogo, MobileIcon}from './NavbarElements.js'
+import {FaBars} from 'react-icons/fa'
+import {NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements.js'
 
 const Navbar = () => {
   return (
     <>
         <Nav>
             <NavbarContainer>
-                <NavLogo to ="/">
-                    Milkhouse
-                </NavLogo>
+                <NavLogo to ="/"> Milkhouse</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to = "home">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to = "about">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to = "services">Services</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to = "contactus">Contact Us</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to = "signin">Sign In</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to = "/signun">Sign Un</NavBtnLink>
+                </NavBtn>
             </NavbarContainer>
         </Nav>
     </>
