8951cd55bcf07fc37e7c02bd5d1e60467a6c8ca4
Mansi <mansins168@gmail.com>
2023-05-31 21:39:52 +0530

    Testimonial added

---
+++ milkhouse_frontend/src/components/Testimonial/TestimonialCSS.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+    margin-bottom: 50px;
+`;
+
+export const ContainerSlider = styled.div`
+    width : 65%;
+`;
+
+export const AvatarHeading = styled.p`
+    font-size: 35px;
+    color: black;
+    font-weight: 500;
+    font-style: italic;
+`
+
+export const AvatarComponent = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    color: gray;
+   
+`
+
+export const AvatarTitle= styled.span`
+    font-weight: 500;
+    color: green;
+
+`;
+
+export const AvatarChar = styled.p`
+    font-style: italic;
+    ${'' /* margin-top: 10px; */}
+`
+export const AvatarContent = styled.p`
+
+`
--- milkhouse_frontend/src/components/Testimonial/index.js
+++ milkhouse_frontend/src/components/Testimonial/index.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import {Avatar} from "@material-ui/core"
+import { Container, ContainerSlider, AvatarComponent , AvatarTitle, AvatarChar, AvatarContent, AvatarHeading} from './TestimonialCSS';
 
 const Testimonial = () => {
   return (
-    <div>Testimonial Slider</div>
+    <Container>
+        <ContainerSlider>
+            <Slider>
+                <Card img="https://www.tutorialrepublic.com/examples/images/clients/1.jpg"/>
+                <Card img="https://www.tutorialrepublic.com/examples/images/clients/2.jpg" />
+                <Card img="https://www.tutorialrepublic.com/examples/images/clients/3.jpg" />
+            </Slider>
+        </ContainerSlider>
+    </Container>
   )
 }
+
+const Card = ({img}) =>{
+    return(
+        <AvatarComponent>
+
+            <AvatarHeading>
+                What our Client says ___
+            </AvatarHeading>
+
+            <Avatar 
+            imgProps={{style: {borderRadius: "50%"}}}
+            src={img} 
+            style={{ width:120, height:120, border: '1px solid lightgray', padding:7}}/>
+
+            <AvatarContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.
+            </AvatarContent>
+
+            <AvatarChar>
+                <AvatarTitle>PAULA WILSON</AvatarTitle>, Dairy Analyst
+            </AvatarChar>
+        </AvatarComponent>
+    );
+};
+
 
 export default Testimonial
