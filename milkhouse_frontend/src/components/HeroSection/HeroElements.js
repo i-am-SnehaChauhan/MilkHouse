2b73c44453d2edb63cbd29d6c23756889cf8d491
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-01 15:16:55 +0530

    Background image set

--- milkhouse_frontend/src/components/HeroSection/HeroElements.js
+++ milkhouse_frontend/src/components/HeroSection/HeroElements.js
@@ -41,11 +41,26 @@ export const HeroContent = motion(styled.div`
     z-index: 3;
     max-width: 1200px;
      position: absolute; 
-   // padding: 8px 24px;
+    padding: 8px 24px;
       display: flex;
     flex-direction: column; 
     align-items: center;
+    
+    :before {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        /* bottom: 0; */
+        left: 0;
+        /* z-index: 2; */
+        background: linear-gradient(
+            180deg,
+            rgba(0,0,0,0.2) 0%,
+            rgba(0,0,0,0.6) 100%
 
+     ),
+          
     `);
 
 export const HeroH1 = styled.h1`
