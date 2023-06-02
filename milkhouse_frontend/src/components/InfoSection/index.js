d779718e68a3399b5c8a01f62fe4b1b909c214e8
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-02 14:40:48 +0530

    Welcome Section Done

--- milkhouse_frontend/src/components/InfoSection/index.js
+++ milkhouse_frontend/src/components/InfoSection/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { InfoContainer, InfoWrapper, InfoRow, Column, TextWrapper, TopLine, Heading, Subtitle } from './InfoElements';
 const InfoSection = () => {
   return (
-    <div>
-      Information Section
-    </div>
+    <>
+      <InfoContainer>
+        <InfoWrapper>
+                <Heading>Welcome to MilkHouse Farm</Heading>
+                <Subtitle>MilkHouse is pleased to welcome you. Here you will find a variety of healthy and fresh dairy
+products that our farmers and cows proudly made on our farm.</Subtitle>
+        </InfoWrapper>  
+      </InfoContainer>
+    </>
   )
 }
 
