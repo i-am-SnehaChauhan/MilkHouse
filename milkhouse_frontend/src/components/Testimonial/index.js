5982aabe4d4125412e5c0861322eb8190170754e
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-09 13:37:45 +0530

    Done

--- milkhouse_frontend/src/components/Testimonial/index.js
+++ milkhouse_frontend/src/components/Testimonial/index.js
@@ -9,7 +9,7 @@ const Testimonial = () => {
   return (
     <Container>
         <ContainerSlider>
-            <Slider>
+            <Slider autoplay={true} autoplaySpeed={3000} dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1}>
                 <Card img="https://www.tutorialrepublic.com/examples/images/clients/1.jpg"/>
                 <Card img="https://www.tutorialrepublic.com/examples/images/clients/2.jpg" />
                 <Card img="https://www.tutorialrepublic.com/examples/images/clients/3.jpg" />
