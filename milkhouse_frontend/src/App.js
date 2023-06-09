3034773fa3594e763308e6a9a9a4d2f066c85d04
Monalika Patnaik <monalikapatnaik114@gmail.com>
2023-06-09 15:49:51 +0530

    updated-changes

--- milkhouse_frontend/src/App.js
+++ milkhouse_frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages';
-//import SignupPage from './pages/signup';
-//import SigninPage from './pages/signin';
+import SignupPage from './pages/signup';
+import SigninPage from './pages/signin';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -12,8 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-       {/* <Route exact path="/signin" element={<SigninPage />} />
-        <Route exact path="/signup" element={<SignupPage />} />*/}
+       <Route exact path="/signin" element={<SigninPage />} />
+        <Route exact path="/signup" element={<SignupPage />} />
         
       </Routes>
     </Router>
--- milkhouse_frontend/src/components/Signin/SigninElements.js
+++ milkhouse_frontend/src/components/Signin/SigninElements.js
@@ -2,129 +2,46 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
- min-height: 692px;
- ${'' /* position: fixed; */}
- padding: 100px;
- bottom: 0;
- left: 0;
- right: 0;
- top: 0;
- z-index: 0;
- overflow: hidden;
- background:var(--bg-clr);
- `;
+height: 1200%;
 
-export const FormWrap = styled.div`
- height: 100%;
- display: flex;
- flex-direction: column;
- justify-content: center;
- `;
-
-export const Icons = styled(Link)`
- margin-left: 32px;
- margin-top: 32px;
- text-decoration: none;
- color: #fff;
- font-weight: 700;
- font-size: 32px;
-
- @media screen and (max-width: 480px) {
-    margin-left: 16px;
-    margin-top: 8px;
- }
+bottom: 0;
+left: 0;
+right: 0;
+top: 20;
+padding: 100px;
+padding-top: 130px;
+padding-bottom: 60px;
+z-index: 0;
+overflow: hidden;
+background:var(--bg-clr);
 `;
 
-export const FormContent = styled.div`
- height: 100%;
- display: flex;
- flex-direction: column;
- justify-content: center;
+export const FormHead = styled.h3`
+  text-align: center;
+  padding: 3px;
+  font-weight:bold;
+  margin-bottom: 30px;
+  width: 100%;
+  @media (min-width: 992px) {
+        width: 50%;
+      }
+`
 
- @media screen and (max-width: 580px) {
-    padding: 10px 20px;
- } 
-`;
+export const FormSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+`
 
-export const Form = styled.form`
- background: linear-gradient(45deg, black,  rgb(1, 147, 86) , rgb(10, 201, 122));
- max-width: 550px;
- height: auto;
- width: 100%;
- z-index: 1;
- display: grid;
- margin: 0 auto;
- padding: 40px 32px;
- border-radius: 4px;
- box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.9);
+export const LeftData = styled.div`
+  width: 100%;
+  padding: 6px;
+`
 
- @media screen and (max-width: 400px) {
-    padding: 20px 32px;
-    height: 81%;
- } 
-`;
+export const RightData = styled.div`
+  width: 100%;
+  margin-top: 3px;
+`
+export const FormP = styled.p`
+  margin-top: 3px;
+`
 
-export const FormH1 = styled.label`
- margin-bottom: 40px;
- color: #fff;
- font-size: 1.4rem;
- font-weight: 800;
- text-align: center;
- `;
- 
- export const FormLabel = styled.label`
- margin-bottom: 8px;
- color: #fff;
- font-size: 1.1rem;
- font-weight: 600;
- cursor:pointer;
-`;
-
-export const FormInput = styled.input`
-margin-bottom: 40px;
-padding: 10px;
-border: none;
-border-radius: 9px;
-font-size:18px;
-border:none;
-outline:none;
-background:rgb(255,255,255);
-:focus{
-   border-bottom:5px solid #01bf71;
-   transition:opacity 0.4s ease;
-   box-shadow:1px 2px 10px #01bf71;
-}
-:invalid{
-   border-bottom:5px solid red;
-}
-@media screen and (max-width:435px){
-   font-size:14px;
-}
-`;
-
-export const FormButton = styled.button`
- background: #01bf71;
- padding: 16px 10px;
- border: none;
- border-radius: 15px;
- color: #fff;
- font-size: 20px;
- cursor: pointer;
- &:hover {
-   opacity : 0.8;
-   transition: opacity 0.25s ease;
- }
- :active{
-   background:#fff;
-   color:#01bf71;
-   opacity:1;
-   border:2px solid #01bf71;
- }
-`;
-
-export const Text = styled.span`
- text-align: center;
- margin-top: 24px;
- color: #fff;
- font-size: 14px;
-`;
