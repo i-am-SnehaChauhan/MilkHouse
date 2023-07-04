91c710b9a3e310d3cfd65849f29733ba39476e5d
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-04 14:08:17 +0530

    Dairy home Page Setup

--- milkhouse_frontend/src/App.js
+++ milkhouse_frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages';
 import SignupPage from './pages/signup';
 import SigninPage from './pages/signin';
 import ContactPage from './pages/contact';
+import DairyPage from './pages/dairy';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -16,6 +17,8 @@ function App() {
        <Route exact path="/signin" element={<SigninPage />} />
         <Route exact path="/signup" element={<SignupPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/dairy" element ={<DairyPage />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Router>
   );
---
+++ milkhouse_frontend/src/pages/dairy.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Ecomponents/Header/Header'
+
+const dairy = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default dairy;
