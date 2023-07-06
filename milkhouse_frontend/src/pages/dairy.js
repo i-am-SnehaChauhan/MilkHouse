fdbd0e4d23512e16ac5f551c6e6dd77858174e9f
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-06 20:21:33 +0530

    Done

--- milkhouse_frontend/src/pages/dairy.js
+++ milkhouse_frontend/src/pages/dairy.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Ecomponents/Header/Header'
+import Home from '../Ecomponents/Home/Home'
 
-
+import {Box} from '@mui/material'
 const dairy = () => {
   return (
     <div>
       <Header />
+      <Box style={{marginTop: 0}} >
+        <Home />
+      </Box>
     </div>
   )
 }
