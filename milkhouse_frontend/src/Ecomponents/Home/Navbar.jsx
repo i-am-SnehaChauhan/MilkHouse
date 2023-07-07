022631eb52f471bd7627bca957ad186514fc5496
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-07 10:22:12 +0530

    Products Section Complete

--- milkhouse_frontend/src/Ecomponents/Home/Navbar.jsx
+++ milkhouse_frontend/src/Ecomponents/Home/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {Box, Typography, styled} from '@mui/material'
+import {navData} from '../../constants/data'
 
+const Component = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  margin: 40px 130px 0px 130px;
+  
+`
+const Container = styled(Box)`
+  padding : 12px 8px;
+  text-align: center;
+`
+
+const Text = styled(Typography)`
+  font-size: 14px;
+  font-weight: 600;
+  font-family: inherit;
+`
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Component>
+      {navData.map((data) => (  
+        <Container>
+          <img src={data.url} alt="logo" style={{width: 74, height: 74}}/> 
+          <Text>{data.text}</Text>
+        </Container>
+      ))}
+    </Component>
+
   )
 }
 
---
+++ milkhouse_frontend/src/constants/data.js
@@ -0,0 +1,11 @@
+export const navData = [
+    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100', text: 'Top Offers' },
+    { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688703545/MilkHouse/Navbar_data/Milk_dra20z.png', text: 'Quality Milk' },
+    { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688704437/MilkHouse/Navbar_data/cheese-clipart-design-illustration-free-png_f0639g.webp', text: 'Cheese' },
+    { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688703537/MilkHouse/Navbar_data/Butter_xpdn8n.png', text: 'Butter' },
+    { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688704651/MilkHouse/Navbar_data/ice-cream-ice-cream-scoops-on-bowl-removebg-preview_n4t7yn.png', text: 'Ice Cream' },
+    { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688705179/MilkHouse/Navbar_data/pngtree-hand-drawn-delicious-yogurt-illustration-image_2300120-removebg-preview_ieffgm.png', text: 'Yogurt' },
+    { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688705486/MilkHouse/Navbar_data/png-transparent-milk-malai-cream-paneer-ghee-milk-food-cheese-cream-cheese-thumbnail-removebg-preview_opkamh.png', text: 'Paneer' },
+    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100', text: 'Travel' },
+    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Beauty, Toys & More' }
+];
