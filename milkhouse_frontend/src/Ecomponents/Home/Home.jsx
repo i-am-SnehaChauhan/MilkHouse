cdcf2cbe4353f33a17b3bae0a5fb5ba282a7e120
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-13 09:43:09 +0530

    Banner Complete

--- milkhouse_frontend/src/Ecomponents/Home/Home.jsx
+++ milkhouse_frontend/src/Ecomponents/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Banner from './Banner';
+import { Box, styled } from '@mui/material';
 
+const Component = styled(Box)`
+   padding: 7px 7px;
+   background-color: #f2f2f2;
+`
 const Home = () => {
   return (
     <>
         <Navbar />
+        <Component>
+            <Banner />
+        </Component> 
+        
+        
+       
+        
     </>
   )
 }
--- milkhouse_frontend/src/constants/data.js
+++ milkhouse_frontend/src/constants/data.js
@@ -7,3 +7,10 @@ export const navData = [
     { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688705179/MilkHouse/Navbar_data/pngtree-hand-drawn-delicious-yogurt-illustration-image_2300120-removebg-preview_ieffgm.png', text: 'Yogurt' },
     { url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1688705486/MilkHouse/Navbar_data/png-transparent-milk-malai-cream-paneer-ghee-milk-food-cheese-cream-cheese-thumbnail-removebg-preview_opkamh.png', text: 'Paneer' },
 ];
+
+export const bannerData = [
+    { id: 1, url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1689220091/MilkHouse/BannerData/banner1_hqtug9.jpg' },
+    { id: 2, url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1689220084/MilkHouse/BannerData/Banner2_tpoimr.jpg' },
+    { id: 3, url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/c_fit,h_280/v1689220122/MilkHouse/BannerData/Screenshot_2023-07-13_091452_z1q6f1.png' },
+    { id: 4, url: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1689220119/MilkHouse/BannerData/Screenshot_2023-07-13_091238_xyhkkc.png' }
+];
