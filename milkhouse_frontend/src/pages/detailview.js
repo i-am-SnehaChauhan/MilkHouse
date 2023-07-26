1e4bb960687395fc20bbb639d41611fb3b1e61b9
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-26 18:22:51 +0530

    Product Rendering

--- milkhouse_frontend/src/pages/detailview.js
+++ milkhouse_frontend/src/pages/detailview.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../Ecomponents/Header/Header'
-import DetailView from '../Ecomponents/Details/DetailView'
+import DetailView from '../Ecomponents/Details/DetailView';
+import {Box, styled} from '@mui/material';
 
+const Component = styled(Box)`
+   padding: 7px 7px;
+   background-color: #f2f2f2;
+`
 const DetailViewPage = () => {
   return (
     <>
       <Header />
-      <DetailView/>
+        <DetailView />
     </>
   )
 }
