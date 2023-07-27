269821041b939d128387ceee1278561b56aef1fd
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-27 19:09:37 +0530

    Search Functionality Done

--- milkhouse_frontend/src/Ecomponents/Header/Search.jsx
+++ milkhouse_frontend/src/Ecomponents/Header/Search.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { InputBase, List, ListItem, Box, styled } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux'; // hooks
-import { getProducts as listProducts } from '../../redux/actions/productAction';
+import { getProducts } from '../../redux/actions/productAction';
 import { Link } from 'react-router-dom';
 
 const SearchContainer = styled(Box)`
@@ -25,6 +25,7 @@ const ListWrapper = styled(List)`
   color: #000;
   background: #FFFFFF;
   margin-top: 36px;
+  z-index: 1;
 `;
 
 const InputSearchBase = styled(InputBase)`
@@ -42,13 +43,12 @@ const Search = () => {
         setOpen(false)
     }
 
-    const getProducts = useSelector(state => state.getProducts);
-    const { products } = getProducts;
+    const { products } = useSelector(state => state.getProducts);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(listProducts())
+        dispatch(getProducts());
     }, [dispatch])
 
     return (
@@ -68,7 +68,7 @@ const Search = () => {
                   products.filter(product => product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product => (
                     <ListItem>
                       <Link 
-                        to={`/product/${product.id}`} 
+                        to={`/dairy/product/${product.id}`} 
                         style={{ textDecoration:'none', color:'inherit'}}
                         onClick={() => setOpen(true)}  
                       >
