42842bfdd6f9c853d14ddaa0c437f860ab3b1ec9
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-08-11 10:07:15 +0530

    cors-resolved

--- milkhouse_backend/index.js
+++ milkhouse_backend/index.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import env from 'dotenv';
-import Connection from './src/database/db.js';
+import express from "express";
+import env from "dotenv";
+import Connection from "./src/database/db.js";
 // import DefaultData from './default.js';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import Routes from './src/routes/route.js';
-
+import bodyParser from "body-parser";
+import cors from "cors";
+import Routes from "./src/routes/route.js";
 
 const app = express();
 
@@ -17,19 +16,20 @@ const password = process.env.MONGO_DB_PASSWORD;
 Connection(user, password);
 
 app.listen(process.env.PORT, () => {
-    console.log('App listening on ' + process.env.PORT);
-    }
-);
+  console.log("App listening on " + process.env.PORT);
+});
 
 // DefaultData();
 
-app.use(express.raw({ type: 'application/json' }));
+app.use(express.raw({ type: "application/json" }));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: ['http://localhost:3000'],
-  };
-  
-  app.use(cors(corsOptions));
-app.use('/', Routes);
+  origin:
+    process.env.NODE_ENV === "production"
+      ? "https://milk-house.vercel.app"
+      : "http://localhost:3000",
+};
 
+app.use(cors(corsOptions));
+app.use("/", Routes);
