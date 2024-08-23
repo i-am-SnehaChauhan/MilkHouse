fce7ca9ca64313cc22218515d74df154ef8c3d25
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-08-23 19:48:45 +0530

    updated-customers-data-in-dashboard

--- milkhouse_frontend/src/Dashboard/Customers.js
+++ milkhouse_frontend/src/Dashboard/Customers.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Loader from "../templates/Loader";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Customers = () => {
   const [customers, setCustomers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Fetch the customers data from the backend
     const fetchCustomers = async () => {
       try {
-        setLoading(true);
-        const response = await fetch(
-          "https://milk-house-azure.vercel.app/customers"
-        );
-        const data = await response.json();
-        setCustomers(data);
-      } catch (error) {
-        console.error("Error fetching customers:", error);
-      } finally {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        const customerData = querySnapshot.docs.map((doc) => ({
+          uid: doc.id,
+          ...doc.data(),
+        }));
+        setCustomers(customerData);
         setLoading(false);
+      } catch (error) {
+        console.error("Error fetching customers: ", error);
       }
     };
 
@@ -30,10 +30,11 @@ const Customers = () => {
   }
 
   return (
+    <>
+    <h1 style={{ textAlign: "center", fontSize: "2.4rem" }}>
+    Customers List
+  </h1>
     <div style={{ display: "block", textAlign: "center", margin: "2rem" }}>
-      <h1 style={{ textAlign: "center", fontSize: "2.4rem" }}>
-        Customers List
-      </h1>
       <table className="rwd-table">
         <tbody>
           <tr>
@@ -41,18 +42,21 @@ const Customers = () => {
             <th>Customer ID</th>
             <th>Customer Name</th>
             <th>Email</th>
+            <th>Phone Number</th>
           </tr>
           {customers.map((customer, index) => (
             <tr key={customer.uid}>
               <td data-th="S.No">{index + 1}</td>
               <td data-th="Customer ID">{customer.uid}</td>
-              <td data-th="Customer Name"></td>
+              <td data-th="Customer Name">{customer.name}</td>
               <td data-th="Email">{customer.email}</td>
+              <td data-th="Phone">{customer.phone}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
+    </>
   );
 };
 
