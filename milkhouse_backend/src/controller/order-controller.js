9a329e86001c390bae11e2f163a151eae55e9cf7
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-10-27 14:12:47 +0530

    added-order-creation-date

--- milkhouse_backend/src/controller/order-controller.js
+++ milkhouse_backend/src/controller/order-controller.js
@@ -1,7 +1,11 @@
 import Order from "../model/orderSchema.js";
 export const getOrders = async(request,response) => {
     try {
-      const orders = await Order.find({});
+        const orders = await Order.find().sort({ createdAt: -1 });
+    //    const formattedOrders = orders.map(order => ({
+    //   ...order,
+    //   createdAt: new Date(order.createdAt).toISOString().split('T')[0]
+    // }));
         response.status(200).json(orders);
     } catch (error) {
         response.status(500).json({message: "Error in Fetching Orders" + error.message});     
--- milkhouse_backend/src/model/orderSchema.js
+++ milkhouse_backend/src/model/orderSchema.js
@@ -14,9 +14,11 @@ const orderSchema = new mongoose.Schema({
   subtotal: { type: Number, required: true },
   total: { type: Number, required: true },
   shipping: { type: Object, required: true },
-  delivery_status: { type: String, default: "pending" },
+  // delivery_status: { type: String, default: "pending" },
   payment_status: { type: String, required: true },
-});
+}, 
+{ timestamps: true }
+);
 const Order = mongoose.model("Order", orderSchema);
 
 export default Order;
