7381184b7f697f03d60875ff96f5316a464ec579
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-11-09 21:39:51 +0530

    razorpay-integration

--- milkhouse_backend/src/model/orderSchema.js
+++ milkhouse_backend/src/model/orderSchema.js
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema({
   customerName: { type: String },
   customerId: { type: String },
-  paymentIntentId: { type: String },
+  // paymentIntentId: { type: String },
   products: [
     {
       id: { type: String },
       title: { type: String },
       quantity: { type: Number },
       mrp: { type: Number },
+      cost: { type: Number },
     },
   ],
-  subtotal: { type: Number, required: true },
   total: { type: Number, required: true },
   shipping: { type: Object, required: true },
   // delivery_status: { type: String, default: "pending" },
-  payment_status: { type: String, required: true },
+  // payment_status: { type: String, required: true },
 }, 
 { timestamps: true }
 );
